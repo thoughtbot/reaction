d68e2c54752e53fecf7698d6070d30119492016a
Joshua Clayton <joshua.clayton@gmail.com>
2019-12-12 12:15:56 -0500

    Allow clusters to react

--- src/Game.elm
+++ src/Game.elm
@@ -45,13 +45,61 @@ type Y
 
 
 advanceBoard : Board -> Board
-advanceBoard (Board particleId particles obstacles) =
-    Board particleId (advanceParticles particles) obstacles
+advanceBoard ((Board _ _ obstacles) as board) =
+    List.foldl handleObstacle board obstacles
+        |> advanceParticles
 
 
-advanceParticles : List Particle -> List Particle
-advanceParticles =
-    List.map advanceParticle
+
+-- Board particleId (advanceParticles particles) obstacles
+-- for each obstacle, check if there's overlap
+-- if there is, do whatever we need to do to each particle overlapping with the obstacle
+--   and also do whatever we need to do with the obstacle
+
+
+particlesAtCoordinates : List Particle -> Coordinates -> List Particle
+particlesAtCoordinates particles coordinates =
+    let
+        particleAtCoordinates coords (Particle _ _ particleCoords) =
+            coords == particleCoords
+    in
+    List.filter (particleAtCoordinates coordinates) particles
+
+
+particlesNotAtCoordinates : List Particle -> Coordinates -> List Particle
+particlesNotAtCoordinates particles coordinates =
+    let
+        particleAtCoordinates coords (Particle _ _ particleCoords) =
+            coords /= particleCoords
+    in
+    List.filter (particleAtCoordinates coordinates) particles
+
+
+handleObstacle : Obstacle -> Board -> Board
+handleObstacle obstacle ((Board particleId particles obstacles) as board) =
+    case obstacle of
+        Cluster (Size n) coordinates ->
+            let
+                excess =
+                    List.length (particlesAtCoordinates particles coordinates) + n - 4
+            in
+            if excess >= 0 then
+                Board
+                    particleId
+                    (particlesNotAtCoordinates particles coordinates ++ List.take excess (particlesAtCoordinates particles coordinates))
+                    (List.filter (\o -> o /= obstacle) obstacles)
+                    |> reactionAt coordinates
+
+            else
+                board
+
+        _ ->
+            board
+
+
+advanceParticles : Board -> Board
+advanceParticles (Board particleId particles obstacles) =
+    Board particleId (List.map advanceParticle particles) obstacles
 
 
 advanceParticle : Particle -> Particle
@@ -72,9 +120,8 @@ advanceParticle (Particle particleId direction (Coordinates (X x) (Y y))) =
 
 initial : Board
 initial =
-    Board initialParticleId [] []
+    Board initialParticleId [] [ Cluster (Size 3) (Coordinates (X 4) (Y 1)), Cluster (Size 3) (Coordinates (X 0) (Y 1)) ]
         |> createParticle Up (Coordinates (X 4) (Y 0))
-        |> reactionAt (Coordinates (X 1) (Y 1))
 
 
 reactionAt : Coordinates -> Board -> Board
